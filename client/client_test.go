1a88f6b60d9589d98f8fac89a0cdc8df38859df0
AJ Bahnken <aj@ajvb.me>
2015-04-20 10:05:20 -0700

    Adds beginning of a client library test suite
    #15

---
+++ client/client_test.go
@@ -0,0 +1,84 @@
+package client
+
+import (
+	"fmt"
+	"net/http/httptest"
+	"time"
+
+	"../api"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var (
+	kc = NewKalaClient("http://127.0.0.1:8080")
+)
+
+func getNewTestServer() *httptest.Server {
+	r := mux.NewRouter()
+	api.SetupApiRoutes(r)
+	return httptest.NewServer(r)
+}
+
+func generateNewJobMap() map[string]string {
+	scheduleTime := time.Now().Add(time.Minute * 5)
+	repeat := 1
+	delay := "P1DT10M10S"
+	parsedTime := scheduleTime.Format(time.RFC3339)
+	scheduleStr := fmt.Sprintf("R%d/%s/%s", repeat, parsedTime, delay)
+
+	return map[string]string{
+		"schedule": scheduleStr,
+		"name":     "mock_job",
+		"command":  "bash -c 'date'",
+		"owner":    "aj@ajvb.me",
+	}
+}
+
+func TestCreateAndGetJob(t *testing.T) {
+	ts := getNewTestServer()
+	kc := NewKalaClient(ts.URL)
+	j := generateNewJobMap()
+
+	id, err := kc.CreateJob(j)
+
+	assert.NoError(t, err)
+	assert.NotEqual(t, id, "")
+
+	respJob, err := kc.GetJob(id)
+
+	assert.NoError(t, err)
+	assert.Equal(t, j["schedule"], respJob.Schedule)
+	assert.Equal(t, j["name"], respJob.Name)
+	assert.Equal(t, j["command"], respJob.Command)
+	assert.Equal(t, j["owner"], respJob.Owner)
+}
+
+func TestGetAllJobs(t *testing.T) {
+	ts := getNewTestServer()
+	kc := NewKalaClient(ts.URL)
+
+	j := generateNewJobMap()
+
+	id, err := kc.CreateJob(j)
+	assert.NoError(t, err)
+
+	jobs, err := kc.GetAllJobs()
+
+	assert.NoError(t, err)
+	assert.Equal(t, j["schedule"], jobs[id].Schedule)
+	assert.Equal(t, j["name"], jobs[id].Name)
+	assert.Equal(t, j["command"], jobs[id].Command)
+	assert.Equal(t, j["owner"], jobs[id].Owner)
+}
+
+func TestDeleteJob(t *testing.T) {
+}
+func TestGetJobStats(t *testing.T) {
+}
+func TestStartJob(t *testing.T) {
+}
+func TestGetKalaStats(t *testing.T) {
+}
