80aa639005136cc7603156fa0c5785c1cad06db6
AJ Bahnken <aj@ajvb.me>
2015-04-25 17:04:30 -0700

    Implemented cli

--- main.go
+++ main.go
@@ -1,31 +1,84 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"./api"
-	//"./ui"
+	"./ui"
 
 	"github.com/222Labs/common/go/logging"
+	"github.com/codegangsta/cli"
 	"github.com/gorilla/mux"
 )
 
 var (
-	log       = logging.GetLogger("kala")
-	staticDir = "/home/ajvb/Code/kala/src/ui/static"
+	log = logging.GetLogger("kala")
+	// TODO - fix
+	staticDir = "/home/ajvb/Code/kala/ui/static"
 )
 
-func main() {
+func initServer() *mux.Router {
 	r := mux.NewRouter()
-
 	// API
 	api.SetupApiRoutes(r)
-
 	// UI
-	//r.HandleFunc("/", ui.HandleDashboard).Methods("GET")
-	//fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir)))
-	//r.PathPrefix("/").Handler(fileServer)
+	r.HandleFunc("/", ui.HandleDashboard).Methods("GET")
+	fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir)))
+	r.PathPrefix("/").Handler(fileServer)
 
-	log.Info("Starting server...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	return r
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Kala"
+	app.Usage = "Modern job scheduler"
+	app.Version = "0.1"
+	app.Commands = []cli.Command{
+		{
+			Name:  "run",
+			Usage: "run kala",
+			Flags: []cli.Flag{
+				cli.IntFlag{
+					Name:  "port, p",
+					Value: 8000,
+					Usage: "port for Kala to run on",
+				},
+				cli.BoolFlag{
+					Name:  "debug, d",
+					Usage: "debug logging",
+				},
+				cli.BoolFlag{
+					Name:  "dont-persist",
+					Usage: "turn off job persistance",
+				},
+			},
+			Action: func(c *cli.Context) {
+				var parsedPort string
+				port := c.Int("port")
+				if port != 0 {
+					parsedPort = fmt.Sprintf(":%d", port)
+				} else {
+					parsedPort = ":8000"
+				}
+
+				// TODO set log level
+				if c.Bool("debug") {
+				}
+
+				// TODO
+				if c.Bool("dont-persist") {
+				}
+
+				r := initServer()
+
+				log.Info("Starting server...")
+				log.Fatal(http.ListenAndServe(parsedPort, r))
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
