fea69740271485c1f63dd19ef0342c7c64673280
AJ Bahnken <aj@ajvb.me>
2015-05-13 17:16:30 -0700

    Fixed logging_test.go

--- utils/logging/logging_test.go
+++ utils/logging/logging_test.go
@@ -3,27 +3,9 @@ package logging
 import (
 	"os"
 
-	logging "github.com/op/go-logging"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
-
-func TestGetLogLevelDefaultHasEnv(t *testing.T) {
-	os.Setenv(LOGLEVEL_ENV_NAME, logging.CRITICAL.String())
-	level := getLogLevelDefault(logging.INFO)
-	assert.Equal(t, logging.CRITICAL, level)
-}
-
-func TestGetLogLevelDefaultBadEnv(t *testing.T) {
-	os.Setenv(LOGLEVEL_ENV_NAME, "$@HJFJ")
-	level := getLogLevelDefault(logging.INFO)
-	assert.Equal(t, logging.INFO, level)
-}
-
-func TestGetLogLevelDefaultUnsetEnv(t *testing.T) {
-	level := getLogLevelDefault(logging.WARNING)
-	assert.Equal(t, logging.WARNING, level)
-}
 
 func TestGetLogHostnameEnvSet(t *testing.T) {
 	hostname := "222labs.com"
