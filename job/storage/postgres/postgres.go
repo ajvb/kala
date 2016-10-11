6215c394bd6b2efaebb21da82e23fab357172653
alpert <alper.tekinalp@gmail.com>
2016-10-12 00:22:55 +0300

    Handle errors more conveniently

--- job/storage/postgres/postgres.go
+++ job/storage/postgres/postgres.go
@@ -1,172 +1,185 @@
 package postgres
 
 import (
-  "fmt"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
 
-  "database/sql"
-  _ "github.com/lib/pq"
-  "github.com/ajvb/kala/job"
+	"database/sql"
+	"github.com/ajvb/kala/job"
+	_ "github.com/lib/pq"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 var (
 	CONN_STRING = "user=%s password=%s dbname=%s sslmode=disable"
 
-	SELECT_QUERY = "SELECT * FROM kala_jobs WHERE id = $1"
-	INSERT_QUERY = "INSERT INTO kala_jobs VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)"
+	SELECT_QUERY     = "SELECT * FROM kala_jobs WHERE id = $1"
+	INSERT_QUERY     = "INSERT INTO kala_jobs VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)"
 	SELECT_QUERY_ALL = "SELECT * FROM kala_jobs"
-	DELETE_QUERY = "DELETE FROM kala_jobs WHERE id = $1"
+	DELETE_QUERY     = "DELETE FROM kala_jobs WHERE id = $1"
 )
 
 type PostgresJobDB struct {
-  conn *sql.DB
+	conn *sql.DB
 }
 
 func New(username string, password string, dbname string) *PostgresJobDB {
-  dbinfo := fmt.Sprintf(CONN_STRING, username, password, dbname)
-  db, err := sql.Open("postgres", dbinfo)
-  checkErr(err)
-  return &PostgresJobDB {
-    conn: db,
-  }
+	dbinfo := fmt.Sprintf(CONN_STRING, username, password, dbname)
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		log.Error("Unable to connect postgresql database.")
+		panic(err)
+	}
+	return &PostgresJobDB{
+		conn: db,
+	}
 }
 
 func (db *PostgresJobDB) Get(id string) (*job.Job, error) {
-  rows, err := db.conn.Query(SELECT_QUERY, id)
-  checkErr(err)
-  defer rows.Close()
+	rows, err := db.conn.Query(SELECT_QUERY, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-  j := new(job.Job)
+	j := new(job.Job)
 
-  for rows.Next() {
-    // name string, id string, command string, owner string, disabled bool, dependent_jobs string[], ParentJobs []string, Schedule string, Retries uint, Epsilon string, NextRunAt time.Time, cd $GOPATH/src/github.com/ajvb/kala
-    var dependentJobsStr string
-    var parentJobsStr string
-    var meta job.Metadata
-    err := rows.Scan(
-      &j.Name, 
-      &j.Id, 
-      &j.Command, 
-      &j.Owner,
-      &j.Disabled,
-      &dependentJobsStr, 
-      &parentJobsStr, 
-      &j.Schedule, 
-      &j.Retries,
-      &j.Epsilon, 
-      &meta.SuccessCount, 
-      &meta.LastSuccess,
-      &meta.ErrorCount, 
-      &meta.LastError, 
-      &meta.LastAttemptedRun, 
-      &j.NextRunAt)
-    checkErr(err)
+	for rows.Next() {
+		// name string, id string, command string, owner string, disabled bool, dependent_jobs string[], ParentJobs []string, Schedule string, Retries uint, Epsilon string, NextRunAt time.Time
+		var dependentJobsStr string
+		var parentJobsStr string
+		var meta job.Metadata
+		err := rows.Scan(
+			&j.Name,
+			&j.Id,
+			&j.Command,
+			&j.Owner,
+			&j.Disabled,
+			&dependentJobsStr,
+			&parentJobsStr,
+			&j.Schedule,
+			&j.Retries,
+			&j.Epsilon,
+			&meta.SuccessCount,
+			&meta.LastSuccess,
+			&meta.ErrorCount,
+			&meta.LastError,
+			&meta.LastAttemptedRun,
+			&j.NextRunAt)
+		if err != nil {
+			return nil, err
+		}
 
-    dependentJobsByte := []byte(dependentJobsStr)
-    json.Unmarshal(dependentJobsByte, &j.DependentJobs);
+		dependentJobsByte := []byte(dependentJobsStr)
+		json.Unmarshal(dependentJobsByte, &j.DependentJobs)
 
-    parentJobsByte := []byte(parentJobsStr)
-    json.Unmarshal(parentJobsByte, &j.ParentJobs);
+		parentJobsByte := []byte(parentJobsStr)
+		json.Unmarshal(parentJobsByte, &j.ParentJobs)
 
-    j.Metadata = meta
-  }
+		j.Metadata = meta
+	}
 
-  return j, nil
+	return j, nil
 }
 
 func (db *PostgresJobDB) Save(j *job.Job) error {
-  dependentJobs, _ := json.Marshal(j.DependentJobs)
-  dependentJobsStr := string(dependentJobs)
+	dependentJobs, _ := json.Marshal(j.DependentJobs)
+	dependentJobsStr := string(dependentJobs)
 
-  parentJobs, _ := json.Marshal(j.ParentJobs)
-  parentJobsStr := string(parentJobs)
+	parentJobs, _ := json.Marshal(j.ParentJobs)
+	parentJobsStr := string(parentJobs)
 
-  row, err := db.conn.Query(INSERT_QUERY, 
-    j.Name, 
-    j.Id, 
-    j.Command, 
-    j.Owner,
-    j.Disabled,
-    dependentJobsStr, 
-    parentJobsStr, 
-    j.Schedule, 
-    j.Retries,
-    j.Epsilon, 
-    j.Metadata.SuccessCount, 
-    j.Metadata.LastSuccess,
-    j.Metadata.ErrorCount, 
-    j.Metadata.LastError, 
-    j.Metadata.LastAttemptedRun, 
-    j.NextRunAt)
-  if row == nil{
-    panic(err)
-  }
-  checkErr(err)
+	row, err := db.conn.Query(INSERT_QUERY,
+		j.Name,
+		j.Id,
+		j.Command,
+		j.Owner,
+		j.Disabled,
+		dependentJobsStr,
+		parentJobsStr,
+		j.Schedule,
+		j.Retries,
+		j.Epsilon,
+		j.Metadata.SuccessCount,
+		j.Metadata.LastSuccess,
+		j.Metadata.ErrorCount,
+		j.Metadata.LastError,
+		j.Metadata.LastAttemptedRun,
+		j.NextRunAt)
+	if row == nil {
+		log.Error(err)
+	}
 
-
-  return err
+	return err
 }
 
 func (db *PostgresJobDB) GetAll() ([]*job.Job, error) {
-  jobs := []*job.Job{}
+	jobs := []*job.Job{}
 
-  rows, err := db.conn.Query(SELECT_QUERY_ALL)
-  checkErr(err)
-  defer rows.Close()
+	rows, err := db.conn.Query(SELECT_QUERY_ALL)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-  for rows.Next() {
-    // name string, id string, command string, owner string, disabled bool, dependent_jobs string[], ParentJobs []string, Schedule string, Retries uint, Epsilon string, NextRunAt time.Time, cd $GOPATH/src/github.com/ajvb/kala
-    j := new(job.Job)
-    
-    var dependentJobsStr string
-    var parentJobsStr string
-    var meta job.Metadata
+	for rows.Next() {
+		// name string, id string, command string, owner string, disabled bool, dependent_jobs string[], ParentJobs []string, Schedule string, Retries uint, Epsilon string, NextRunAt time.Time
+		j := new(job.Job)
 
-    err := rows.Scan(
-      &j.Name, 
-      &j.Id, 
-      &j.Command, 
-      &j.Owner,
-      &j.Disabled,
-      &dependentJobsStr, 
-      &parentJobsStr, 
-      &j.Schedule, 
-      &j.Retries,
-      &j.Epsilon, 
-      &meta.SuccessCount, 
-      &meta.LastSuccess,
-      &meta.ErrorCount, 
-      &meta.LastError, 
-      &meta.LastAttemptedRun, 
-      &j.NextRunAt)
-    checkErr(err)
+		var dependentJobsStr string
+		var parentJobsStr string
+		var meta job.Metadata
 
-    dependentJobsByte := []byte(dependentJobsStr)
-    json.Unmarshal(dependentJobsByte, &j.DependentJobs);
+		err := rows.Scan(
+			&j.Name,
+			&j.Id,
+			&j.Command,
+			&j.Owner,
+			&j.Disabled,
+			&dependentJobsStr,
+			&parentJobsStr,
+			&j.Schedule,
+			&j.Retries,
+			&j.Epsilon,
+			&meta.SuccessCount,
+			&meta.LastSuccess,
+			&meta.ErrorCount,
+			&meta.LastError,
+			&meta.LastAttemptedRun,
+			&j.NextRunAt)
+		if err != nil {
+			return nil, err
+		}
 
-    parentJobsByte := []byte(parentJobsStr)
-    json.Unmarshal(parentJobsByte, &j.ParentJobs);
+		dependentJobsByte := []byte(dependentJobsStr)
+		json.Unmarshal(dependentJobsByte, &j.DependentJobs)
 
-    j.Metadata = meta
+		parentJobsByte := []byte(parentJobsStr)
+		json.Unmarshal(parentJobsByte, &j.ParentJobs)
 
-    jobs = append(jobs, j)
-  }
+		j.Metadata = meta
 
-  return jobs, nil
+		jobs = append(jobs, j)
+	}
+
+	return jobs, nil
 }
 
 func (db *PostgresJobDB) Delete(id string) error {
-  _, err := db.conn.Exec(DELETE_QUERY, id)
-  checkErr(err)
-  return err
+	_, err := db.conn.Exec(DELETE_QUERY, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (db *PostgresJobDB) Close() error {
-  return db.conn.Close()
-}
+	err := db.conn.Close()
+	if err != nil {
+		return err
+	}
 
-func checkErr(err error) {
-  if err != nil {
-    panic(err)
-  }
+	return nil
 }
