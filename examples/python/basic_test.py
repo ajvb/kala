302e3020d958688a0e53f09df1fd2313709f6f0f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-12-28 14:42:57 +0100

    Remove spaces

--- examples/python/basic_test.py
+++ examples/python/basic_test.py
@@ -17,19 +17,19 @@ dt = datetime.isoformat(datetime.now(tzlocal()) + timedelta(0, 10))
 data["schedule"] = "%s/%s/%s" % ("R2", dt, "PT10S")
 
 if __name__ == "__main__":
-    print ('Sending request to {0}'.format(API_URL))
-    print ('Payload is: {0}'.format(data))
+    print('Sending request to {0}'.format(API_URL))
+    print('Payload is: {0}'.format(data))
     r = requests.post(API_URL, data=simplejson.dumps(data))
 
     print("\n\nCreating...\n")
     job_id = simplejson.loads(r.content)['id']
-    print ('Job was created with an id of {0}'.format(job_id))
+    print('Job was created with an id of {0}'.format(job_id))
 
     print("\n\nGetting...\n")
     m = requests.get(API_URL + job_id)
     print(m.content)
 
-    print ("\n\nDeleting...\n")
+    print("\n\nDeleting...\n")
 
     print("\n\nWaiting to delete...\n")
     sleep(21)
