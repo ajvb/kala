3d0870cb9ef4677d4ba865167540fd5e2ab232f1
MattMc <4984708+tooolbox@users.noreply.github.com>
2020-05-26 12:37:32 -0700

    Add load testing scripts.

---
+++ load/spiketest.js
@@ -0,0 +1,70 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import { Trend } from 'k6/metrics';
+
+export let options = {
+    stages: [
+        // Ramp-up from 1 to 5 VUs in 10s
+        {
+            duration: "10s",
+            target: 5
+        },
+
+        // Stay at rest on 5 VUs for 5s
+        {
+            duration: "5s",
+            target: 200
+        },
+
+        // Ramp-down from 5 to 0 VUs for 5s
+        {
+            duration: "5s",
+            target: 5
+        },
+        // Ramp-down from 5 to 0 VUs for 5s
+        {
+            duration: "10s",
+            target: 1
+        }
+    ]
+};
+
+let jobs = new Trend('jobs');
+let activeJobs = new Trend('active_jobs');
+let errorCount = new Trend('error_count');
+let successCount = new Trend('success_count');
+
+var count = 0;
+
+function getSchedule(now) {
+    now.setSeconds(now.getSeconds() + 1);
+    return 'R5/' + now.toISOString() + '/PT5S';
+}
+
+export default function() {
+    let payload = JSON.stringify({
+        name: 'job' + (count++),
+        schedule: getSchedule(new Date()),
+        command: "bash -c 'date'"
+    });
+    let write = http.post("http://localhost:8000/api/v1/job/", payload);
+    check(write, {
+        "status is ok": (r) => r.status === 201
+    });
+    sleep(1);
+    let read = http.get("http://localhost:8000/api/v1/job/");
+    check(read, {
+        "status is ok": (r) => r.status === 200
+    });
+    sleep(1);
+    let stats = http.get("http://localhost:8000/api/v1/stats/");
+    check(stats, {
+        "status is ok": (r) => r.status === 200
+    });
+    let parsed = JSON.parse(stats.body);
+    jobs.add(parsed.Stats.jobs);
+    activeJobs.add(parsed.Stats.active_jobs);
+    errorCount.add(parsed.Stats.error_count);
+    successCount.add(parsed.Stats.success_count);
+    sleep(1);
+}
