d7153e0b2ff36b4f764652541dcf15f9323b9c90
Jose Juan Montiel <josejuan.montiel@gmail.com>
2021-03-10 16:25:04 +0100

    Fix postgres persistence

--- job/storage/postgres/postgres.go
+++ job/storage/postgres/postgres.go
@@ -36,6 +36,7 @@ func New(dsn string) *DB {
 // GetAll returns all persisted Jobs.
 func (d DB) GetAll() ([]*job.Job, error) {
 	query := fmt.Sprintf(`select coalesce(json_agg(j.job), '[]'::json) from (select * from %[1]s) as j;`, TableName)
+
 	var r sql.NullString
 	err := d.conn.QueryRow(query).Scan(&r)
 	if err != nil && err != sql.ErrNoRows {
@@ -46,12 +47,13 @@ func (d DB) GetAll() ([]*job.Job, error) {
 	if r.Valid {
 		err = json.Unmarshal([]byte(r.String), &jobs)
 	}
+
 	return jobs, err
 }
 
 // Get returns a persisted Job.
 func (d DB) Get(id string) (*job.Job, error) {
-	template := `select to_jsonb(j.job) from (select * from %[1]s where job -> 'id' = $1) as j;`
+	template := `select to_jsonb(j.job) from (select * from %[1]s where job ->> 'id' = $1) as j;`
 	query := fmt.Sprintf(template, TableName)
 	var r sql.NullString
 	err := d.conn.QueryRow(query, id).Scan(&r)
@@ -67,14 +69,20 @@ func (d DB) Get(id string) (*job.Job, error) {
 
 // Delete deletes a persisted Job.
 func (d DB) Delete(id string) error {
-	query := fmt.Sprintf(`delete from %v where job -> = 'id' = $1;`, TableName)
+	query := fmt.Sprintf(`delete from %v where job ->> 'id' = $1;`, TableName)
 	_, err := d.conn.Exec(query, id)
 	return err
 }
 
 // Save persists a Job.
 func (d DB) Save(j *job.Job) error {
-	template := `insert into %[1]s (job) values($1);`
+	template := `update %[1]s SET job = $1 where job ->> 'id' = '` + j.Id + `'`
+
+	_, errFind := d.Get(j.Id)
+	if errFind != nil {
+		template = `insert into %[1]s (job) values($1);`
+	}
+
 	query := fmt.Sprintf(template, TableName)
 	r, err := json.Marshal(j)
 	if err != nil {
