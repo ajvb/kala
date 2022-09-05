c8603729e03d14c72b334d2d0756fe3848453c64
Jose Juan Montiel <josejuan.montiel@gmail.com>
2022-09-05 07:44:00 +0200

    needed too?

--- job/job_test.go
+++ job/job_test.go
@@ -1096,6 +1096,8 @@ func TestRemoteJobBadStatus(t *testing.T) {
 	})
 
 	cache := NewMockCache()
+	mockRemoteJob.Init(cache)
+	cache.Start(0, 2*time.Second) // Retain 1 minute
 
 	mockRemoteJob.Run(cache)
 	assert.True(t, mockRemoteJob.Metadata.SuccessCount == 0)
